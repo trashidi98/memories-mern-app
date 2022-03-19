f6695c2800ace15ff705553b20a052edecf5cc0a
trashidi98 <tabish.rashidi@gmail.com>
2022-03-19 13:17:31 -0400

    Adding in some material UI

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 import useStyles from "./styles"; 
+import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAltIcon'; 
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'; 
 
 const Post = () => {
     const classes = useStyles(); 
